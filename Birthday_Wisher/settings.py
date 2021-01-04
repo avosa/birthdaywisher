c957ee9f5f585cb5f18bd9a547d1f06bdcf075f7
avosa <websterb17@gmail.com>
2021-01-04 22:09:15 +0300

    bugs fixes

--- Birthday_Wisher/settings.py
+++ Birthday_Wisher/settings.py
@@ -135,8 +135,8 @@ STATIC_URL = '/static/'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('avosa.info@gmail.com')
-EMAIL_HOST_PASSWORD = config('@365office')
+EMAIL_HOST_USER = config('avosa.info@gmail.com',  default='localhost')
+EMAIL_HOST_PASSWORD = config('@365office', default='')
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 
